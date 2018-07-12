14221ac9f0c90c84439ffd5522da3e33dbd97c4d
izep <5875073+izep@users.noreply.github.com>
2018-07-11 19:54:38 -0600

    ready for initial testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 const { spawn } = require("child_process");
-const http = require("http");
+const http = require("https");
 const fs = require("fs");
+const { exec } = require("child_process");
+
 
 console.info(`Starting programs...`);
 const optionsLoc = "../running/options.json";
@@ -10,10 +12,10 @@ const sleep = function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const downloadSettings = function () {
+const downloadFile = function (path, url) {
     return new Promise((resolve) => {
-        let file = fs.createWriteStream(optionsLoc);
-        var request = http.get("http://raw.githubusercontent.com/izep/pi-signs/master/scripts/options.json", function (response) {
+        let file = fs.createWriteStream(path);
+        var request = http.get(url, function (response) {
             response.pipe(file);
             file.on('error', function (err) {
                 console.error(err);
@@ -27,26 +29,23 @@ const downloadSettings = function () {
     });
 }
 
-const downloadMedia = function (name, location) {
-    file = fs.createWriteStream(videoFile);
-    var request = http.get(
-        "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg",
-        function (response) {
-            response.pipe(file);
-            file.on("error", function (err) {
-                fs.unlink(dest);
-            });
-        }
-    );
-
-}
-
-
 async function main() {
-    await downloadSettings();
+    await downloadFile(optionsLoc, "https://raw.githubusercontent.com/izep/pi-signs/master/scripts/options.json");
     let optsStr = fs.readFileSync(optionsLoc);
     let opts = JSON.parse(optsStr);
-    console.log(optsStr);
+    console.log(JSON.stringify(opts));
+    let mediaPromises = [];
+    opts.files.forEach(itm => {
+        let dl = downloadFile(mediaLoc + itm.name, itm.url);
+        mediaPromises.push(dl);
+    });
+    mediaPromises.forEach(async itm => {
+        await itm;
+    });
+
+    exec(`omxplayer -o local --loop --orientation 270  ${mediaLoc}${opts[0].name}`);
+
+    console.log('exiting.');
 }
 
 main();
